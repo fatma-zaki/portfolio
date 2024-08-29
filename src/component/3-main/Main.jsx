9118593f70ced707e36684a4d73ec3a2ed071a94
elsa7aby <fatmazaki712@gmail.com>
2024-08-30 02:30:48 +0300

    main section

--- src/component/3-main/Main.jsx
+++ src/component/3-main/Main.jsx
@@ -1,9 +1,46 @@
-import './main.css'
+import "./main.css";
 
 function Main() {
   return (
-    <div>Main</div>
-  )
+    <main className="flex">
+      <section className="flex left-section">
+        <button className="active">all projects</button>
+        <button>HTML & Css</button>
+        <button>javascript</button>
+        <button>react & mui</button>
+        <button>node & express</button>
+      </section>
+      <section className="right-section flex">
+        {
+          ["aa", "dd", "ss",1,1].map(item => {
+            return(
+        <article key={item} className="card">
+          <img src="./landing-page.jpg" alt="" />
+          <div className="box">
+            <h2>landing page</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Necessitatibus, quo illo. Aspernatur exercitationem laborum eos,
+            </p>
+            <div className="flex icons">
+              <div className="flex">
+                <div className="icon-link"></div>
+                <div className="icon-github"></div>
+              </div>
+              <a className="flex" href="#">
+                more
+                <span className="icon-arrow_forward"></span>
+              </a>
+            </div>
+          </div>
+        </article>
+
+            )
+          })
+        }
+      </section>
+    </main>
+  );
 }
 
-export default Main
+export default Main;
