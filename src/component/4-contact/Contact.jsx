9d9167bf68029ff4f50b1417b201e7a41172f195
elsa7aby <fatmazaki712@gmail.com>
2024-09-05 01:47:59 +0300

    contact design

--- src/component/4-contact/Contact.jsx
+++ src/component/4-contact/Contact.jsx
@@ -1,9 +1,32 @@
-import './contact.css'
+import "./contact.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="contact-us">
+      <h2 className="title">
+        <span className="icon-envelope"></span>contact us
+      </h2>
+      <p className="sub-title">
+        contact us for more information and get notified when i publish some
+        thing new
+      </p>
+      <div className="flex">
+        <form >
+          <div className="flex">
+            <label htmlFor="email">email address:</label>
+            <input required type="email" name="" id="email" />
+          </div>
 
-export default Contact
+          <div style={{marginTop: "24px"}} className="flex">
+            <label htmlFor="message">your message:</label>
+            <textarea required name="" id="message"></textarea>
+          </div>
+          <button className="submit">submit</button>
+        </form>
+        <div className="animation"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
