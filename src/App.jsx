e008d8f64143215d9f774d7cec4515a7b04b0db3
elsa7aby <fatmazaki712@gmail.com>
2024-09-06 13:56:47 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./component/1-header/Header"
 import Hero from "./component/2-hero/Hero"
 import Main from "./component/3-main/Main"
 import Contact from "./component/4-contact/Contact"
-import { Footer } from "./component/5-footer/Footer"
+import Footer from "./component/5-footer/Footer"
 
 function App() {
   
--- src/component/5-footer/Footer.jsx
+++ src/component/5-footer/Footer.jsx
@@ -1,7 +1,17 @@
 import './footer.css'
 
-export const Footer = () => {
+const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flex'>
+      <ul className='flex'>
+        <li><a href="">about</a></li>
+        <li><a href="">projects</a></li>
+        <li><a href="">speaking</a></li>
+        <li><a href="">uses</a></li>
+      </ul>
+
+    <p> © 2024 all rights reserved</p>
+    </footer>
   )
 }
+export default Footer;
